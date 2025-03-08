b05eec6f65472233deccd528573876cdbabd1521
Pearlitox <151786530+Pearlitox@users.noreply.github.com>
2025-03-08 17:46:21 +0100

    ajout texture + animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,80 +22,156 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render( scene, camera );
 
-
 let topbun;
 let topmeat;
 let pickles;
-let salad
+let salad;
 let middlebun;
 let bottommeat;
 let botsalad;
 let cheese;
 let bottombun; 
+//lights
+
+const light = new THREE.AmbientLight( 0xffffff, 1, 100);
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+directionalLight.castShadow = true;
 
 
-const material = new THREE.MeshBasicMaterial({color : "white"});
+scene.add( light, directionalLight);
+//grouper les mesh
+const group = new THREE.Group();
+
+//models
 const loader = new GLTFLoader();
 
 loader.load('./assets/models/topbun.glb',function (gltf){
     topbun = gltf.scene;
     topbun.traverse((child) => {
         if (child.isMesh) {
-            child.material = material;
+            child.material = new THREE.MeshToonMaterial({color: '#E29348' })
         }
     });
     console.log('done');
-    topbun.scale.set(1,1,1);
-    topbun.position.z = -4;
-    scene.add(topbun);
+ 
+  
+    
+    group.add(topbun);
     animate();
   });
   loader.load('./assets/models/topmeat.glb',function (gltf){
     topmeat = gltf.scene;
     topmeat.traverse((child) => {
         if (child.isMesh) {
-            child.material = material;
+            child.material = new THREE.MeshToonMaterial({color: '#6C3F29' })
         }
     });
     console.log('done');
-    topmeat.scale.set(1,1,1);
-    topmeat.position.z = -4;
-    scene.add(topmeat);
+  
+  
+    group.add(topmeat);
     animate();
   });
   loader.load('./assets/models/pickles.glb',function (gltf){
     pickles = gltf.scene;
     pickles.traverse((child) => {
         if (child.isMesh) {
-            child.material = material;
+            child.material = new THREE.MeshToonMaterial({color: '#7F711F' })
         }
     });
     console.log('done');
-    pickles.scale.set(1,1,1);
-    pickles.position.z = -4;
-    scene.add(pickles);
+   
+
+    group.add(pickles);
     animate();
   });
 loader.load('./assets/models/topsalad.glb',function (gltf){
     salad = gltf.scene;
     salad.traverse((child) => {
         if (child.isMesh) {
-            child.material = material;
+            child.material = new THREE.MeshToonMaterial({color: '#7BB654' })
         }
     });
     console.log('done');
-    salad.scale.set(1,1,1);
-    salad.position.z = -4;
-    scene.add(salad);
+    
+
+    group.add(salad);
     animate();
   });
+  loader.load('./assets/models/midbun.glb',function (gltf){
+    middlebun = gltf.scene;
+    middlebun.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshToonMaterial({color: '#E29348' })
+        }
+    });
+    console.log('done');
+
+  
+    group.add(middlebun);
+    animate();
+  });
+  loader.load('./assets/models/botmeat.glb',function (gltf){
+    bottommeat = gltf.scene;
+    bottommeat.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshToonMaterial({color: '#6C3F29' })
+        }
+    });
+    console.log('done');
 
 
+    group.add(bottommeat);
+    animate();
+  });
+  loader.load('./assets/models/cheese.glb',function (gltf){
+    cheese = gltf.scene;
+    cheese.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshToonMaterial({color: '#FFD61F' })
+        }
+    });
+    console.log('done');
+ 
+
+    group.add(cheese);
+    animate();
+  });
+  loader.load('./assets/models/botsalad.glb',function (gltf){
+    botsalad = gltf.scene;
+    botsalad.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshToonMaterial({color: '#7BB654' })
+        }
+    });
+    console.log('done');
+ 
+
+    group.add(botsalad);
+    animate();
+  });
+  loader.load('./assets/models/botbun.glb',function (gltf){
+    bottombun = gltf.scene;
+    bottombun.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshToonMaterial({color: '#E29348' })
+        }
+    });
+    console.log('done');
+  
+
+    group.add(bottombun);
+    animate();
+    scene.add(group);
+    group.position.z = -4
+  });
+  
 
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    group.rotation.y+=0.01;
 }
 animate();
 
